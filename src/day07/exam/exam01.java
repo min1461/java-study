2a6f4e4d74d7488173ec3b68cd265215e22beeeb
min1461 <kangminkue777@gmail.com>
2020-09-22 19:59:02 +0900

    for문 추가 수정!

--- src/day07/exam/exam01.java
+++ src/day07/exam/exam01.java
@@ -7,8 +7,10 @@ public class exam01 {
 		Scanner in = new Scanner(System.in);
 		System.out.println("입력한 수 만큼 hellojava를 출력하세요 : ");
 		int n = in.nextInt();
-		for (int i = 0; i < n; i++) {
-			System.out.println(i + 1 + ". hellojava");
+		int i = 1;
+		for (; i <= n; i++) {
+			// for문 안에 초기값을 '(int)i' 지정하면 for문이 종료되었을때 값이 없어짐 (지역변수개념)
+			System.out.println(i + ". hellojava");
 		}
 	}
 }
